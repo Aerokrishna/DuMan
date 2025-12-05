7b045a6040c8b8e55fd64d1dc9bcb90171e1289a
Krishnapranav <krishnapranav.221me227@nitk.edu.in>
2025-12-05 22:00:07 +0530

    picknplace server

---
+++ pkgs/duman_control/control/picknplace_left.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionServer, ActionClient
+from rclpy.action.server import ServerGoalHandle, GoalResponse, CancelResponse
+from duman_interfaces.action import PickNPlace, DumanGoal
+from duman_interfaces.srv import GripState
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+import threading
+from rclpy.action.client import ClientGoalHandle, GoalStatus
+from rclpy.callback_groups import ReentrantCallbackGroup
+import time
+from objects import objects_left
+import copy
+
+# it performs sequential pick and place
+
+# it is responsible for cancelling the request if necessary
+# this server reads from the camera and object poses, requests replan
+# gripping retry tree
+# retry if planning fails multiple times
+
+class PicknPlace(Node):
+    def __init__(self):
+        super().__init__("move_duman_left")
+        self.goal_handle_ : ServerGoalHandle = None
+
+        self.goal_lock_ = threading.Lock()
+        # create action client
+        self.duman_left_goal_client_ = ActionClient(self, DumanGoal, "/duman/goal_left", callback_group=ReentrantCallbackGroup())
+
+        self.joint_goal_server_ = ActionServer(
+            self, 
+            PickNPlace,  
+            "/duman/pnp_left",
+            goal_callback=self.goal_callback, 
+            cancel_callback=self.cancel_callback, 
+            execute_callback=self.pnp_callback, 
+            callback_group=ReentrantCallbackGroup()) 
+        
+        self.duman_grip_client = self.create_client(GripState, "/duman/grip_state", callback_group=ReentrantCallbackGroup())
+
+        self.arm_done = False
+        self.state = 0
+        self.goal_sent = False
+
+        self.object_poses = objects_left
+        self.object_height = 0.05 # 5cm
+        self.approach_ht = 0.1
+
+        self.get_logger().info("duman left pick and place server")
+
+        self.last_time = time.monotonic()
+        self.cnt = 0
+
+        self.get_logger().info("waiting for server....")
+        self.duman_left_goal_client_.wait_for_server() # you can provide a timer to wait for the server inside
+        self.get_logger().info("server found!")
+
+    def goal_callback(self, goal_request: PickNPlace.Goal):
+        
+        # reject the goal if a goal is already executing
+        with self.goal_lock_:
+            if self.goal_handle_ is not None and self.goal_handle_.is_active:
+                self.get_logger().error("GOAL ACTIVE...rejecting new goal")
+                return GoalResponse.REJECT
+
+        return GoalResponse.ACCEPT
+
+    def cancel_callback(self, goal_handle: ServerGoalHandle): # goal handle is going to be cancelled as client will cancel a particular goal
+        self.get_logger().info("cancel request received...")
+        return CancelResponse.ACCEPT
+    
+    def pnp_callback(self, goal_handle : ServerGoalHandle):
+        self.state = 1
+        # self.get_logger().info("PASSING REQUEST RECEIVED")
+        with self.goal_lock_:
+            self.goal_handle_ = goal_handle
+
+        obj_pose = copy.deepcopy(self.object_poses[goal_handle.request.object_id])
+        align_pose = copy.deepcopy(self.object_poses[goal_handle.request.object_id])
+        align_pose[2] += (self.object_height + self.approach_ht)
+
+        while True:
+            time.sleep(0.1)
+            # if self.delay_(0.1):
+            if self.state == 1:
+                self.get_logger().info(f"left Arm Aligning to object ")
+
+                if not self.goal_sent:
+                    self.arm_done = False
+                    
+                    self.send_goal(arm=False, goal_type=True, target=align_pose)
+                    self.goal_sent = True
+            
+            elif self.state == 2:
+                self.get_logger().info(f"Moving towards object")
+
+                if not self.goal_sent:
+                    self.arm_done = False
+                    """
+                    OBJECT POSE
+                    """
+                    self.send_goal(arm=False, goal_type=True, target=obj_pose)
+                    self.goal_sent = True
+
+            elif self.state == 3 and self.delay_(1.0):
+
+                if not self.goal_sent:
+                    self.get_logger().info(f"Grip Command")
+
+                    # if pick is true grip state is true means close the gripper else false then open
+                    self.send_grip_cmd(arm=False, grip_state=goal_handle.request.pick)
+                    self.goal_sent = True
+
+            elif self.state == 4 and self.delay_(2.0):
+                self.get_logger().info(f"left Arm Aligning to object ")
+
+                if not self.goal_sent:
+                    self.arm_done = False
+
+                    self.send_goal(arm=False, goal_type=True, target=align_pose)
+                    self.goal_sent = True
+                    
+            if self.state == 5:
+                self.get_logger().info(f"IDLING")
+                self.state = 0
+                self.goal_sent = False
+                self.goal_handle_ = None
+
+                break
+        
+        result = PickNPlace.Result()
+        result.success = True
+        result.message = "Pose Goal Succeeded"
+
+        goal_handle.succeed()
+        self.get_logger().info("GOAL FINISH RETURNING SUCCESS!")
+
+        return result
+    
+    def send_goal(self, arm, goal_type, target):
+
+        # Define your goal as your custom action
+        goal = DumanGoal.Goal()
+
+        goal.arm = arm #left arm
+        goal.object_id = "default"
+
+        if goal_type == 0:
+            goal.goal_type = goal_type #joint goal
+            goal.hip = target[0]
+            goal.shoulder = target[1]
+            goal.elbow = target[2]
+            goal.wrist1 = target[3]
+            goal.wrist2 = target[4]
+            goal.wrist3 = target[5]
+
+            # self.get_logger().info("JOINT Goal sending")
+        
+        else:
+            goal.goal_type = goal_type #joint goal
+            goal.x = target[0]
+            goal.y = target[1]
+            goal.z = target[2]
+            goal.orx = target[3]
+            goal.ory = target[4]
+            goal.orz = target[5]
+
+            # self.get_logger().info("POSE Goal sending")
+
+        self.duman_left_goal_client_.send_goal_async(goal).add_done_callback(self.goal_response_callback) 
+
+    def send_grip_cmd(self, arm, grip_state):
+        # Create a request for the ArucoSW service, to get the pick and drop coordinates.
+
+        req = GripState.Request()
+        req.grip_state = grip_state
+        req.arm = arm
+
+        if arm==False:
+            self.get_logger().info("GRIPPER left!")
+
+            # Call the service asynchronously
+            future = self.duman_grip_client.call_async(req)
+            future.add_done_callback(self.grip_result_callback)
+        
+        else :
+            # Call the service asynchronously
+            self.get_logger().info("GRIPPER LEFT!")
+
+            future = self.duman_grip_client.call_async(req)
+            future.add_done_callback(self.grip_result_callback)
+    
+    def grip_result_callback(self, future):
+        try:
+            if future.result().success:
+                self.state += 1
+                self.goal_sent = False
+
+            # self.get_logger().info(f'Service response: {self.response}')
+        except Exception as e:
+            self.get_logger().error(f'Service call failed: {e}')
+
+    def goal_response_callback(self, future):
+        # callback to see if goal was accpeted
+        self.goal_handle_: ClientGoalHandle = future.result()
+
+        if self.goal_handle_.accepted:
+            # self.get_logger().info("GOAL ACCEPTED!")
+
+            # add a callback which runs when a result is received
+            self.goal_handle_.get_result_async().add_done_callback(self.motion_result_callback) # call the future callback
+        else:
+            self.get_logger().warn("GOAL REJECTED")
+
+    # a callback to signify completion of an arm motion task
+    # the motion result will include which arm has completed the motion task
+    def motion_result_callback(self,future):
+        status = future.result().status
+        result = future.result().result # is the reached number interface made in actions
+
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.state+=1
+            self.goal_sent = False
+            
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("ABORTED")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().error("CANCELLED")
+
+        # self.get_logger().info(f"Result : {result.message} {result.success}")  #+ str(result.reached_number)
+    
+    def cancel_goal(self):
+        self.get_logger().info("Sending cancel request")
+        self.goal_handle_.cancel_goal_async()
+
+    def delay_(self, period):
+        current_time = time.monotonic()
+        if current_time - self.last_time >= period:
+            self.last_time = current_time
+            return True
+        return False
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = PicknPlace()
+    executor = MultiThreadedExecutor(num_threads=2)
+    executor.add_node(node)
+    executor.spin()
+    rclpy.shutdown()
+
+if __name__=="__main__":
+    main()
+
